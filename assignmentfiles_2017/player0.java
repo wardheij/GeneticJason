c5f1a9f2b40c419e421d8cd27465891ba3d2baf2
Wouter <wouter-vrielink@hotmail.com>
2017-09-20 19:42:50 +0200

    testing changed and HC added

--- assignmentfiles_2017/player0.java
+++ assignmentfiles_2017/player0.java
@@ -9,12 +9,12 @@ public class player0 implements ContestSubmission
 	Random rnd_;
 	ContestEvaluation evaluation_;
     private int evaluations_limit_;
-	
+
 	public player0()
 	{
 		rnd_ = new Random();
 	}
-	
+
 	public void setSeed(long seed)
 	{
 		// Set seed of algortihms random process
@@ -25,41 +25,84 @@ public class player0 implements ContestSubmission
 	{
 		// Set evaluation problem used in the run
 		evaluation_ = evaluation;
-		
+
 		// Get evaluation properties
 		Properties props = evaluation.getProperties();
-        // Get evaluation limit
-        evaluations_limit_ = Integer.parseInt(props.getProperty("Evaluations"));
+    // Get evaluation limit
+    evaluations_limit_ = Integer.parseInt(props.getProperty("Evaluations"));
 		// Property keys depend on specific evaluation
 		// E.g. double param = Double.parseDouble(props.getProperty("property_name"));
-        boolean isMultimodal = Boolean.parseBoolean(props.getProperty("Multimodal"));
-        boolean hasStructure = Boolean.parseBoolean(props.getProperty("Regular"));
-        boolean isSeparable = Boolean.parseBoolean(props.getProperty("Separable"));
+    boolean isMultimodal = Boolean.parseBoolean(props.getProperty("Multimodal"));
+    boolean hasStructure = Boolean.parseBoolean(props.getProperty("Regular"));
+    boolean isSeparable = Boolean.parseBoolean(props.getProperty("Separable"));
 
 		// Do sth with property values, e.g. specify relevant settings of your algorithm
-        if(isMultimodal){
-            // Do sth
-        }else{
-            // Do sth else
-        }
+    if(isMultimodal){
+        // Do sth
+    }else{
+        // Do sth else
     }
-    
+  }
+
 	public void run()
 	{
 		// Run your algorithm here
-        
-        int evals = 0;
-        // init population
-        // calculate fitness
-        while(evals<evaluations_limit_){
-            // Select parents
-            // Apply crossover / mutation operators
-            double child[] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
-            // Check fitness of unknown fuction
-            Double fitness = (double) evaluation_.evaluate(child);
-            evals++;
-            // Select survivors
-        }
+
+		hillClimber();
+
+    // int evals = 0;
+    // // init population
+    // // calculate fitness
+    // while(evals<evaluations_limit_){
+    //     // Select parents
+    //     // Apply crossover / mutation operators
+    //     double child[] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+    //     // Check fitness of unknown fuction
+    //     Double fitness = (double) evaluation_.evaluate(child);
+    //     evals++;
+    //     // Select survivors
+    // }
 
 	}
+
+	public void hillClimber()
+	{
+		double currBest[] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+		double currBestFitness = (double) evaluation_.evaluate(currBest);
+
+		// We already did one eval..
+		for(int evals = 1; evals < evaluations_limit_; evals++)
+		{
+		    double child[] = sumArray(currBest, randomArray(10));
+
+		    double fitness = (double) evaluation_.evaluate(child);
+
+		    if (fitness > currBestFitness) {
+		    	currBestFitness = fitness;
+					currBest = child;
+		    }
+		}
+
+	}
+
+	public double[] randomArray(int n){
+			double arr[] = new double[n];
+
+			for (int i = 0; i < n; i++) {
+				// arr[i] = rnd_.nextDouble() - 0.5;
+				arr[i] = rnd_.nextGaussian();
+			}
+
+			return arr;
+	}
+
+	public double[] sumArray(double[] m, double[] n)
+	{
+		for (int i = 0; i < m.length; i++) {
+			n[i] += m[i];
+		}
+
+		return n;
+	}
+
 }
--- assignmentfiles_2017/simple_test.sh
+++ assignmentfiles_2017/simple_test.sh
@@ -1,3 +1,13 @@
 javac -cp contest.jar player0.java
 jar cmf MainClass.txt submission.jar player0.class
+echo "Sphere: "
+java -jar testrun.jar -submission=player0 -evaluation=SphereEvaluation -seed=1
+echo ""
+echo "Bent Cigar: "
 java -jar testrun.jar -submission=player0 -evaluation=BentCigarFunction -seed=1
+echo ""
+echo "Schaffers: "
+java -jar testrun.jar -submission=player0 -evaluation=SchaffersEvaluation -seed=1
+echo ""
+echo "Katsuura: "
+java -jar testrun.jar -submission=player0 -evaluation=KatsuuraEvaluation -seed=1
